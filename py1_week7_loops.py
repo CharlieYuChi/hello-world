e6f9a694c8ed95b66f161fd218e3f01629c5f5a9
YuchiChen <yucharlie.chen@gmail.com>
2017-06-29 00:03:39 +0800

    revise py1_week7_loops

--- py1_week7_loops.py
+++ py1_week7_loops.py
@@ -1,2 +1,24 @@
 #practice loops
 
+largest = None
+smallest = None
+first = True
+while True:
+    num = input("Enter a number: ")
+    if num == "done" : break
+    try:
+    	num = int(num)
+    except:
+        print('Invalid input')
+        continue
+    if first == True:
+        largest = num
+        smallest = num
+        first = False
+    if num > largest:
+        largest = num
+    if num < smallest:
+        smallest = num
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
